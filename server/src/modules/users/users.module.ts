e61d09a47ca716f2b4f9da36b5ac11c8495a0d4a
okoloboga <okoloboga37@gmail.com>
2025-07-25 16:14:54 +0300

    docker fix, server impor fix user module

--- server/src/modules/users/users.module.ts
+++ server/src/modules/users/users.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { UsersController } from './users.controller'
 import { UsersService } from './users.service'
 import { User } from '../../entities/user.entity'
+import { Referral } from '../../entities/referrals.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Referral])],
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
