27fb1c3dd5e34f5a3df900fbedbb5dbfbf3d229b
okoloboga <okoloboga37@gmail.com>
2025-07-28 11:31:39 +0300

    fix: resolve all dependency injection issues

--- server/src/modules/finances/finances.module.ts
+++ server/src/modules/finances/finances.module.ts
@@ -4,11 +4,18 @@ import { FinancesService } from './finances.service';
 import { FinancesController } from './finances.controller';
 import { Deposit } from '../../entities/deposits.entity';
 import { User } from '../../entities/user.entity';
+import { ApiService } from '../../services/api.service';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Deposit, User])],
+  imports: [
+    TypeOrmModule.forFeature([Deposit, User]),
+    BullModule.registerQueue({
+      name: 'callback-queue',
+    }),
+  ],
   controllers: [FinancesController],
-  providers: [FinancesService],
+  providers: [FinancesService, ApiService],
   exports: [FinancesService],
 })
 export class FinancesModule {}
