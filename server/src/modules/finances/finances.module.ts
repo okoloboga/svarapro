508cf22b04875d00f372b5f842cf771e4630a564
okoloboga <okoloboga37@gmail.com>
2025-07-28 11:22:49 +0300

    fix: resolve dependency injection issues

--- server/src/modules/finances/finances.module.ts
+++ server/src/modules/finances/finances.module.ts
@@ -4,11 +4,12 @@ import { FinancesService } from './finances.service';
 import { FinancesController } from './finances.controller';
 import { Deposit } from '../../entities/deposits.entity';
 import { User } from '../../entities/user.entity';
+import { ApiService } from '../../services/api.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Deposit, User])],
   controllers: [FinancesController],
-  providers: [FinancesService],
+  providers: [FinancesService, ApiService],
   exports: [FinancesService],
 })
 export class FinancesModule {}
