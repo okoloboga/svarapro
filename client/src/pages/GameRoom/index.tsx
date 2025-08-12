18684888ef77edf258c20d1e778a20c80cf2bef5
okoloboga <okoloboga37@gmail.com>
2025-08-12 18:19:22 +0300

    style(client): move game table up with negative margin

--- client/src/pages/GameRoom/index.tsx
+++ client/src/pages/GameRoom/index.tsx
@@ -211,7 +211,7 @@ export function GameRoom({ roomId, socket, setCurrentPage, userData }: GameRoomP
       {/* Игровой стол и места для игроков */}
       <div className="flex-grow relative p-4 z-10">
         {/* Центральный контейнер для стола и позиций игроков */}
-        <div className="relative flex justify-center items-center min-h-[70vh] w-full p-4 sm:p-5 lg:p-6 game-table-container">
+        <div className="relative flex justify-center items-center min-h-[70vh] w-full p-4 sm:p-5 lg:p-6 game-table-container -mt-16">
           {/* Контейнер стола с позиционированием игроков */}
           <div className="relative flex justify-center items-center w-full h-full">
             {/* Игровой стол */}
