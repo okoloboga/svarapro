97a614c046905883e05fa76ef9a51d0477558c9f
okoloboga <okoloboga37@gmail.com>
2025-08-13 12:48:48 +0300

    corrected players positions and table position

--- client/src/components/GameProcess/PlayerSpot.tsx
+++ client/src/components/GameProcess/PlayerSpot.tsx
@@ -177,7 +177,7 @@ export function PlayerSpot({ player, isCurrentUser, showCards, scale = 1 }: Play
         
         {/* Card deck, positioned to the right and overlapping */}
         {!hasFolded && (
-          <div className="absolute top-1/2 right-0 transform -translate-y-1/2 z-30" style={{ top: '30%', left: '50px' }}>
+          <div className="absolute top-1/2 right-0 transform -translate-y-1/2 z-30" style={{ top: '40%', left: '50px' }}>
             <div className="relative" style={{ width: '42px', height: '42px' }}>
               <img
                 src={cardBack}
