beabb457ef0f4dc8ff306ef87a762ead597115d3
okoloboga <okoloboga37@gmail.com>
2025-08-15 15:58:49 +0300

    card position and button vision fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -232,7 +232,7 @@ function App() {
       ) : currentPage === 'depositHistory' ? (
         <DepositHistory setCurrentPage={handleSetCurrentPage} userId={String(userData.id)} />
       ) : currentPage === 'gameRoom' && pageData && pageData.roomId ? (
-        <GameRoom roomId={pageData.roomId} balance={parseFloat(balance)} socket={socket} setCurrentPage={handleSetCurrentPage} userData={userData} pageData={pageData} />
+        <GameRoom roomId={pageData.roomId} balance={balance} socket={socket} setCurrentPage={handleSetCurrentPage} userData={userData} pageData={pageData} />
       ) : (
         <Dashboard
           onMoreClick={() => handleSetCurrentPage('more')}
